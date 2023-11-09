50926d5dec87e4ca2a85de0b1753475d892f1252
emanuellefurmanovicz <128699187+emanuellefurmanovicz@users.noreply.github.com>
2023-11-09 15:58:03 -0300

    lknlnlnnnlkn

--- aula21/forms_fibo.php
+++ aula21/forms_fibo.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,28 +8,80 @@
     <title>Document</title>
 </head>
 <style>
-    *{
-    background-color: rgb(255, 119, 210);
+    * {
+        background-color: rgb(255, 119, 210);
     }
-    #bt{
+
+    #bt {
         text-align: center;
-        
+
+    }
+
+    h1 {
+        text-align: center;
+    }
+
+    #posi {
+        font: size 20px;
+
+        color: white;
     }
 </style>
-<body>
-  <div class="row mt-2">
-      <div class="col-6 offset-3">
-          <form method="post">
-          <label>$posicao</label>
-          <input name="nome:" type="text" class="form-control"
-          placeholder="sequencia de fibonacci" value="<?php if(isset($_POST["posicao"])) echo $_POST["posicao"]; ?>/>/>
-          <div id="bt">
-          <button type="button" class="btn btn-outline-primary">enviar</button>
-          <button type="button" class="btn btn-outline-danger">resetar</button>
-          </div>
-      </div>
-  </div>
-  </form>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-</html>
 
+<body>
+<?php
+ $fibon = array();
+ $fibonacci[0]= 1;
+ $fibonacci[1]= 1;
+
+ function fibo($fibon){
+ $fibonacci = array();
+ $fibonacci[0]= 1;
+ $fibonacci[1]= 1;
+
+ for ($valor = 2; $valor <$fibon; $valor++){
+     $fibonacci[$valor] = $fibonacci[$valor - 1] + $fibonacci[$valor - 2];
+ }
+ return $fibonacci[$fibon - 1];}
+
+ ?>
+    <h1>calculadora</h1>
+    <div class="row mt-2">
+        <div class="col-6 offset-3">
+            <?php
+            if (isset($_POST["posicao"])) {
+            ?>
+                <h1>resultado</h1>
+                <div class="row mt-4">
+                    <div class="col-5 ">
+                        <div class="alert alert-dark" role="alert">
+                        <small> posiçao: </small>
+                        <h2></? echo $_POST["posicao"]; ?></h2>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                    <div class="col-5 offset-2">
+                         <div class="alert alert-dark" role="alert">
+                           <small> valor Y: </small>
+                           <h2><?php echo fibo($_POST["posicao"]); ?></h2>
+                        </div>
+                    </div>
+                </div>
+            <?php
+            }
+            ?>
+
+            <form method="post">
+                <label>posicao</label>
+                <input name="posicao" type="text" class="form-control" placeholder="sequencia de fibonacci" value="<?php if (isset($_POST["posicao"])) echo $_POST["posicao"]; ?>" />
+                <div id="bt">
+                    <button type="submit" class="btn btn-outline-primary">enviar</button>
+                    <button type="button" class="btn btn-outline-danger">resetar</button>
+                </div>
+        </div>
+    </div>
+
+    </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
+</html>
